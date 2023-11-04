9c33cc6999d5d106f31210327e81a4f6f8dfc9e0
David <davidisadev@gmail.com>
2023-11-04 14:54:14 -0500

    fix: removes redefinition of os.
    ruff F811

--- e_filetypes_py/helpers.py
+++ e_filetypes_py/helpers.py
@@ -3,7 +3,6 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-import os
 import base64
 import json
 
